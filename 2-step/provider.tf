ede9b85872e71fe3f013058f34497892b1046f2f
rahulwagh <rahul.wagh@jhooq.com>
2023-11-28 22:04:13 +0200

    some renaming

--- 2-step/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region                   = "eu-central-1"
-  shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
-}
--- 2-step/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "bucket_name" {
-  type        = string
-  description = "Remote state bucket name"
-}
