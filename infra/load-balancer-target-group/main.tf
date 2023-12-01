63a0100b7b63e98a520ab1f01cd85dff0967d42d
rahulwagh <rahul.wagh@jhooq.com>
2023-12-01 22:57:29 +0200

    Added health status end point

--- infra/load-balancer-target-group/main.tf
+++ infra/load-balancer-target-group/main.tf
@@ -13,10 +13,19 @@ resource "aws_lb_target_group" "dev_proj_1_lb_target_group" {
   port     = var.lb_target_group_port
   protocol = var.lb_target_group_protocol
   vpc_id   = var.vpc_id
+  health_check {
+    path = "/health"
+    port = 5000
+    healthy_threshold = 6
+    unhealthy_threshold = 2
+    timeout = 2
+    interval = 5
+    matcher = "200"  # has to be HTTP 200 or fails
+  }
 }
 
 resource "aws_lb_target_group_attachment" "dev_proj_1_lb_target_group_attachment" {
   target_group_arn = aws_lb_target_group.dev_proj_1_lb_target_group.arn
   target_id        = var.ec2_instance_id
-  port             = 80
+  port             = 5000
 }
